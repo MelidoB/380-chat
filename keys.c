4acd675e260ce4104cb2d610f77faa28f3acc8d6
melido <enrriquebello10@gmail.com>
2024-05-12 22:40:52 -0400

    Refactored key management code for clarity and consistency, simplifying file handling, removing unnecessary header inclusions, and updating function parameter types to use struct dhKey consistently.

--- keys.c
+++ keys.c
@@ -4,20 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include "util.h"
-#include <openssl/sha.h>
 
-int initKey(dhKey* k)
+int initKey(struct dhKey* k)
 {
 	assert(k);
 	mpz_init(k->PK);
 	mpz_init(k->SK);
-	/* NOTE: PK == SK == 0 at this point */
 	strncpy(k->name,"default",MAX_NAME);
 	return 0;
 }
 
-int shredKey(dhKey* k)
+int shredKey(struct dhKey* k)
 {
 	assert(k);
 	size_t nLimbs = mpz_size(k->SK);
@@ -37,7 +34,7 @@ int shredKey(dhKey* k)
  * (where A = g^a)
  * */
 
-int writeDH(char* fname, dhKey* k)
+int writeDH(char* fname, struct dhKey* k)
 {
 	assert(k);
 	/* NOTE if fname was already PATH_MAX-3 or longer, the name will be
@@ -51,17 +48,13 @@ int writeDH(char* fname, dhKey* k)
 	strncpy(fnamepub,fname,PATH_MAX);
 	strncat(fnamepub,".pub",PATH_MAX);
 	/* when saving secret key, make sure file isn't world-readable */
-	int fd;
-	FILE* f;
-	if (mpz_cmp_ui(k->SK,0)) { /* SK present so write it */
-		fd = open(fname,O_RDWR|O_CREAT|O_TRUNC,0600);
-		f = fdopen(fd,"wb");
-		if (!f) return -1;
-		fprintf(f, "name:%s\n", k->name);
-		gmp_fprintf(f, "pk:%Zd\n", k->PK);
-		gmp_fprintf(f, "sk:%Zd\n", k->SK);
-		fclose(f);
-	}
+	int fd = open(fname,O_RDWR|O_CREAT|O_TRUNC,0600);
+	FILE* f = fdopen(fd,"wb");
+	if (!f) return -1;
+	fprintf(f, "name:%s\n", k->name);
+	gmp_fprintf(f, "pk:%Zd\n", k->PK);
+	gmp_fprintf(f, "sk:%Zd\n", k->SK);
+	fclose(f);
 	f = fopen(fnamepub,"wb");
 	if (!f) return -1;
 	fprintf(f, "name:%s\n", k->name);
@@ -71,7 +64,7 @@ int writeDH(char* fname, dhKey* k)
 	return 0;
 }
 
-int readDH(char* fname, dhKey* k)
+int readDH(char* fname, struct dhKey* k)
 {
 	assert(k);
 	initKey(k);
@@ -79,13 +72,12 @@ int readDH(char* fname, dhKey* k)
 	if (!f) return -1;
 	int rv = 0;
 	char* name;
-	/* TODO %ms might not be portable?  Also might not read spaces. */
+	/* XXX make sure %ms is working properly */
 	if (fscanf(f,"name:%ms\n",&name) != 1) {
 		rv = -2;
 		goto end;
 	}
 	strncpy(k->name,name,MAX_NAME);
-	k->name[MAX_NAME] = 0; /* make sure it's a c-string */
 	free(name);
 	if (gmp_fscanf(f,"pk:%Zd\n",k->PK) != 1) {
 		rv = -2;
@@ -98,20 +90,4 @@ int readDH(char* fname, dhKey* k)
 end:
 	fclose(f);
 	return rv;
-}
-
-char* hashPK(dhKey* k, char* hash)
-{
-	assert(k);
-	const size_t hlen = 32; /* byte len of binary hash */
-	unsigned char H[hlen]; /* buffer for binary hash */
-	size_t nB;
-	unsigned char* buf = Z2BYTES(NULL,&nB,k->PK);
-	SHA256(buf,nB,H);
-	char hc[17] = "0123456789abcdef";
-	if (!hash) hash = malloc(2*hlen);
-	for (size_t i = 0; i < 2*hlen; i++) {
-		hash[i] = hc[((H[i/2] << 4*(i%2)) & 0xf0) >> 4];
-	}
-	return hash;
 }
